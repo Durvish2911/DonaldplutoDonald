aacc84c2225f8cf2696b1421bc0ebc6c36eea868
Durvish2911 <72204035+Durvish2911@users.noreply.github.com>
2021-07-16 17:09:25 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,9 +3,9 @@ Webcam.set({
     width:350,
     image_format:"png",
     png_quality:100,
-    constrainbts:(
-        facingMode:"environment"
-    )
+    constrainbts:{
+facingMode:"enviroment"
+    }
 });
 var camera=document.getElementById("cam");
 Webcam.attach('  #cam  ');
@@ -18,4 +18,16 @@ console.log("ml5 version",ml5.version);
 var varh=ml5.imageClassifier("MobileNet",hello);
 function hello(){
     console.log("modeltoaded")
+}
+function heck(){
+    var maam=document.getElementById("cap_img");
+    varnmala.classify(maam,gotreselta)
+}
+function gotreselta(error,results){
+    if(error){
+        console.error(error);
+    }else{
+    console.log(results);
+    document.getElementById("object_name").innerHTML=results[0].label
+    }
 }
